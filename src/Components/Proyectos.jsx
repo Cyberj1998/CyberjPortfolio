644a541685730bfc47dbd9a0ee7c6e5b3046183a
Cyberj1998 <159168306+Cyberj1998@users.noreply.github.com>
2024-11-05 09:56:40 -0500

    Add files via upload

--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-//----------------import Tech---------------------------
+//---------------------import Tech-------------------------------
 import ReactLogo from './Assets/React.png'
 import TailwindcssLogo from './Assets/Tailwindcss.png'
 import AnimejsLogo from './Assets/Anime.png'
@@ -69,7 +69,7 @@ function Proyectos () {
                     <div>
                         <h3 className="font-sans font-semibold text-[20px] bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Headphones Page</h3>
                         <p className="text-[#757575]">modern landing page for a headphones company featuring sleek design elements and smooth animations using <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Tailwind CSS</span>, <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Anime.js</span> and <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Framer Motion</span>. 'add to cart' functionality using <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Redux</span> to handle complex states.</p>
-                        <div className="border border-grey-500 h-[50px] flex justify-left items-center rounded-[5px]">
+                        <div className="border border-grey-500 h-fit flex flex-wrap justify-left items-center rounded-[5px]">
                             <img src={ReactLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={TailwindcssLogo} className="w-[40px] h-[30px] m-[5px]" alt="react" />
                             <img src={AnimejsLogo} className="w-[40px] h-[30px] rounded-[5px] m-[5px]" alt="react" />
@@ -86,7 +86,7 @@ function Proyectos () {
                     <div>
                         <h3 className="font-sans font-semibold text-[20px] bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">3D Interactive Website</h3>
                         <p className="text-[#757575]">This React project showcases a stunning <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">3D model</span> of a floating island using <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Three.js</span> and <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">React-Three-Fiber</span>. The user can interact with the 3D model by rotating it using drag-and-drop gestures</p>
-                        <div className="border border-grey-500 h-[50px] flex justify-left items-center rounded-[5px]">
+                        <div className="border border-grey-500 h-fit flex flex-wrap justify-left items-center rounded-[5px]">
                             <img src={ViteLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={ThreeJS} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={ReactLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
@@ -102,7 +102,7 @@ function Proyectos () {
                     <div>
                         <h3 className="font-sans font-semibold text-[20px] bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Anime Infinite Scroll</h3>
                         <p className="text-[#757575]"><span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Next.js</span> project to showcase the <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">server-side</span> actions, we also use the <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Framer Motion</span> library to create nice and smooth scroll animations</p>
-                        <div className="border border-grey-500 h-[50px] flex justify-left items-center rounded-[5px]">
+                        <div className="border border-grey-500 h-fit flex flex-wrap justify-left items-center rounded-[5px]">
                             <img src={NextLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={TailwindcssLogo} className="w-[40px] h-[30px] m-[5px]" alt="react" />
                             <img src={FramerMotionLogo} className="w-[40px] h-[30px] rounded-[5px] m-[5px]" alt="react" />
@@ -117,7 +117,7 @@ function Proyectos () {
                     <div>
                         <h3 className="font-sans font-semibold text-[20px] bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Node.js API/Landing Page</h3>
                         <p className="text-[#757575]"><span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Node.js</span> API that everyone can use to build amazing projects. Alongside that, I built this attractive landing page with <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Three.js</span> and Redux to showcase what you can do with this <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">API</span>.</p>
-                        <div className="border border-grey-500 h-[50px] flex justify-left items-center rounded-[5px]">
+                        <div className="border border-grey-500 h-fit flex flex-wrap justify-left items-center rounded-[5px]">
                             <img src={ViteLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={ReactLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={TailwindcssLogo} className="w-[40px] h-[30px] rounded-[5px] m-[5px]" alt="react" />
@@ -136,7 +136,7 @@ function Proyectos () {
                     <div>
                         <h3 className="font-sans font-semibold text-[20px] bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">T-Shirt Customization</h3>
                         <p className="text-[#757575]">Modern T-shirt customization page with <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">3D interactive model</span> thanks to the power of <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Three.js</span>, and modern style using <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Tailwind CSS</span> and <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Framer Motion</span>.</p>
-                        <div className="border border-grey-500 h-[50px] flex justify-left items-center rounded-[5px]">
+                        <div className="border border-grey-500 h-fit flex flex-wrap justify-left items-center rounded-[5px]">
                             <img src={ViteLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={ReactLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={TailwindcssLogo} className="w-[40px] h-[30px] rounded-[5px] m-[5px]" alt="react" />
@@ -153,7 +153,7 @@ function Proyectos () {
                     <div>
                         <h3 className="font-sans font-semibold text-[20px] bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">E-Commerce with Redux & Emailjs</h3>
                         <p className="text-[#757575]">Full E-Commerce website with <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">node.js</span> server to store and load the products, add to cart functions with <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Redux</span> 3d models with <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">ThreeJs</span> and modern animations using <span className="bg-gradient-to-r from-[#3d74ff] to-[#3dd8ff] text-transparent bg-clip-text">Framer Motion</span></p>
-                        <div className="border border-grey-500 h-[50px] flex justify-left items-center rounded-[5px]">
+                        <div className="border border-grey-500 h-fit flex flex-wrap justify-left items-center rounded-[5px]">
                             <img src={ViteLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={ReactLogo} className="w-[40px] h-[40px] m-[5px]" alt="react" />
                             <img src={TailwindcssLogo} className="w-[40px] h-[30px] rounded-[5px] m-[5px]" alt="react" />
@@ -178,6 +178,8 @@ function Proyectos () {
         </div>
     )
 }
+
+
 
 
 
