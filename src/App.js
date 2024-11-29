97698f94dea58b4bfb6b1c42b83473156336cc6e
Cyberj1998 <159168306+Cyberj1998@users.noreply.github.com>
2024-11-29 10:27:35 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Hero from './Components/Hero'
 import Proyectos from './Components/Proyectos'
 import DeveloperSection from './Components/DeveloperSection';
 import Contact from './Components/Contact'
-import Loader from './Components/Loader';
 import Email from './Components/Email';
 
 
@@ -16,14 +15,6 @@ function App() {
   return (
     <div className="App">
           <>
-            {
-              loading ? (
-                <Loader />
-              )
-              : (
-                ''
-              )
-            }
             <Hero />
             <Proyectos />
             <DeveloperSection loading={loading} setLoading={setLoading} />
