5d4bb06d82d8ae8169bbdd3edb1e0f40ff194311
Cyberj1998 <159168306+Cyberj1998@users.noreply.github.com>
2025-02-08 14:56:08 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['Roboto', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
+
+
 
